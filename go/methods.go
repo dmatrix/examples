cad0ddfdf8c64b27d166424016301ffe1f22c44c
Jules S. Damji <dmatrix@comcast.net>
2015-10-25 14:24:52 -0700

    added methods file

---
+++ go/methods.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//declare type struct in this file. Consider this as a class to which we will define some methods
+//a Vertex struct with two coordinates
+//
+type Vertex struct {
+	X, Y float64
+}
+//methods can be assigned to any type your package. 
+type MyInt int
+//let's define a method for it, say cube it 
+// define a method for this type. 
+// the method name is Abs() and it returns float64, while receving a pointer to type type struct Vertex
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+//let's define a method for it, say cube it 
+func (x MyInt) cube() int {
+	return int(x * x * x)
+}
+
+//let's see how they work
+func main() {
+	//assisgn vertices to Vertex and then let v point to the address of Vertext (pointer)
+	v := &Vertex{3, 4}
+	// use v and inovke its method, just as you would if it were a class or object with methods.
+	fmt.Println(v.Abs())
+	//defining an instance of your type MyInt, with value integer 5
+	x := MyInt(5)
+
+	fmt.Println(x.cube())
+
+}

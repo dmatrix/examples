763000e5ad0cc0fe7e85c38a1ef788a655235910
Jules S. Damji <dmatrix@comcast.net>
2015-11-10 17:17:26 -0800

    added geo location messages

--- pubnub/py/pub_sub.py
+++ pubnub/py/pub_sub.py
@@ -12,7 +12,7 @@ pubnub = Pubnub(publish_key="demo", subscribe_key="demo")
 #
 # define some callbacks 
 #
-def callback(message, channel):
+def sub_callback(message, channel):
 	print (message)
 
 def error(message):
@@ -20,13 +20,24 @@ def error(message):
 
 def connect(message):
 	print ("CONNECTED")
-	messages = ["Hello World from PubNub's Python SDK", 
+	messages = [
+				"Hello World from PubNub's Python SDK", 
 				"Not bad for a quick examples", 
-				"TTYL...",
-				"{'time': '2:07:04pm', 'price': '510.83', 'delta': '69.02', 'perc': '13.51', 'vol': 2930}"]
+				"Some Geo location data:",
+				"{ 'vehicle_id: 10, 'lat': -37.123, 'lng':120.456}",
+				"{ 'vehicle_id: 11, 'lat': -38.123, 'lng':121.456}",
+				"{ 'vehicle_id: 12, 'lat': -39.123, 'lng':122.456}",
+				"{ 'vehicle_id: 13, 'lat': -40.123, 'lng':123.456}",
+				"Here are some stock quotes:",
+				"{'stock': 'APPL', time': '2:07:04pm', 'price': '510.83', 'delta': '69.02', 'perc': '13.51', 'vol': 2930}",
+				"{'stock': 'IBM', time': '2:07:04pm', 'price': '610.83', 'delta': '79.02', 'perc': '15.51', 'vol': 293000}",
+				"{'stock': 'CISCO', time': '2:07:04pm', 'price': '530.83', 'delta': '89.02', 'perc': '83.51', 'vol': 42930}",
+				"{'stock': 'HP', time': '2:07:04pm', 'price': '510.83', 'delta': '69.02', 'perc': '13.51', 'vol': 32930}",
+				"{'stock': 'GOOGL', time': '2:07:04pm', 'price': '610.83', 'delta': '69.02', 'perc': '13.51', 'vol': 930}",
+				"TTYL..." 
+				]
 	for msg in messages:
 		pubnub.publish(channel="my_channel", message=msg)
-		print "Napping..."
 		time.sleep(1)
 
 def reconnect(message):
@@ -38,6 +49,6 @@ def disconnect(message):
 # subscribe to a channel and invoke the appropriate callback when a message arrives on that 
 # channel
 #
-pubnub.subscribe(channels="my_channel", callback=callback, error=callback,
+pubnub.subscribe(channels="my_channel", callback=sub_callback, error=sub_callback,
 					connect=connect, reconnect=reconnect, disconnect=disconnect)
-sys.exit(0)
+

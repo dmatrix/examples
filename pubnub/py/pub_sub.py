1a1245d821ddd92376b332a8e7b9382cc92199a5
Jules S. Damji <dmatrix@comcast.net>
2015-11-10 16:19:22 -0800

    added JSON messages and array of messages to publish

--- pubnub/py/pub_sub.py
+++ pubnub/py/pub_sub.py
@@ -1,12 +1,16 @@
 
 #!/usr/env python
-from pubnub import pubnub
+from pubnub import Pubnub
+import time
+import sys
 #
-
-pubnub = Pubnub(publlish_key="demo", subscribe_key="demo")
+# initate the Pubnub instance
+# subsistute your publish_key and subscribe_key here
+#
+pubnub = Pubnub(publish_key="demo", subscribe_key="demo")
 
 #
-# define some call backs
+# define some callbacks 
 #
 def callback(message, channel):
 	print (message)
@@ -16,13 +20,24 @@ def error(message):
 
 def connect(message):
 	print ("CONNECTED")
-	pubnub.publish(channel="my_channel", message="Hello World from PubNub's Python SDK")
+	messages = ["Hello World from PubNub's Python SDK", 
+				"Not bad for a quick examples", 
+				"TTYL...",
+				"{'time': '2:07:04pm', 'price': '510.83', 'delta': '69.02', 'perc': '13.51', 'vol': 2930}"]
+	for msg in messages:
+		pubnub.publish(channel="my_channel", message=msg)
+		print "Napping..."
+		time.sleep(1)
 
 def reconnect(message):
 	print ("RECONNECTED")
 
 def disconnect(message):
 	print ("DISCONNECTED")
-
+#
+# subscribe to a channel and invoke the appropriate callback when a message arrives on that 
+# channel
+#
 pubnub.subscribe(channels="my_channel", callback=callback, error=callback,
 					connect=connect, reconnect=reconnect, disconnect=disconnect)
+sys.exit(0)

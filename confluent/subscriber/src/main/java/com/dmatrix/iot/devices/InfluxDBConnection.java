8a7b80399dc1456c1b27519ba2fbe523af70021d
Jules S. Damji <dmatrix@comcast.net>
2015-12-19 13:42:20 -0800

    added InfluxDBConneciton Singleton class

---
+++ confluent/subscriber/src/main/java/com/dmatrix/iot/devices/InfluxDBConnection.java
@@ -0,0 +1,44 @@
+/**
+ * Singleton class for InfluxDB
+ *
+ * author:Jules S. Damji
+ */
+
+import org.influxdb.*;
+import org.influxdb.dto.Point;
+
+public final class InfluxDBConnection {
+
+    private static InfluxDBConnection instance = null;
+    private static InfluxDB influxDB = null;
+    private String user = "jules";
+    private String pwd  = "influxdb";
+    private static String db   = "pubnub_devices";
+
+    /**
+     *  private constructor so no other class or thread can instantiate it
+     */
+    private InfluxDBConnection() throws Exception {
+       try {
+           influxDB = InfluxDBFactory.connect("http://localhost:8086", user, pwd);
+       } catch (Exception ex) {
+           ex.printStackTrace();
+           throw new Exception(String.format("Failed to Connect to InfluxDB on the localhost:8086 with user='%s'/password='%s'", user, "******"));
+       }
+    }
+    /**
+     *
+     * @return the single instance to InfluxDB
+     */
+    public static InfluxDBConnection getInstance() throws Exception {
+        if(instance == null) {
+            instance = new InfluxDBConnection();
+        }
+        return instance;
+    }
+
+    public static void writePoints(Point pt) {
+        influxDB.write(db, "default", pt);
+    }
+
+}

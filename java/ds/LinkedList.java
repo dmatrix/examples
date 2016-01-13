61967fa5052ad8a8ec66b10a351d6352d7552392
Jules S. Damji <dmatrix@comcast.net>
2016-01-12 18:46:14 -0800

    added more methods to linkedlist

--- java/ds/LinkedList.java
+++ java/ds/LinkedList.java
@@ -1,6 +1,8 @@
 package ds;
 
 /**
+ * A linked list implementation, which really is Queue, since elments are appended to the end, while elements
+ * are removed from the front. FIFO
  * Created by jules on 1/12/16.
  */
 public class LinkedList {
@@ -8,7 +10,7 @@ public class LinkedList {
     //instance variables for the LinkedList
     private Node first, last;
     private int elements;
-    
+
     /**
      * Constructor for the linked list
      */
@@ -35,6 +37,12 @@ public class LinkedList {
     }
 
     /**
+     * Return number of elements in the list
+     * @return elments in the list
+     */
+    public int size() { return elements; }
+
+    /**
      *
      * @param node
      * @return
@@ -49,6 +57,21 @@ public class LinkedList {
             last.setNext(node);
             last = node;
         }
+        elements++;
+        return node;
+    }
+
+    /**
+     * Remove elment from the front of th list.
+     * @return
+     */
+    public Node remove() {
+        Node node = getFirst();
+        if (!isEmpty()) {
+            first = first.next();
+            elements--;
+        }
+
         return node;
     }
 
@@ -64,17 +87,57 @@ public class LinkedList {
     }
 
     /**
+     * @param device
+     * @return Node with device name, device, otherwise return null
+     */
+    public Node findNode(String device) {
+        Node link = first;
+        Node found = null;
+        if (!isEmpty()) {
+            while (link.next() != null) {
+                if (link.equals(device)) {
+                    found = link;
+                    break;
+                } else {
+                    link = link.next();
+                }
+            }
+        }
+        return found;
+    }
+
+    /**
      * Main driver of the program
      * @param args
      */
     public static void main(String[] args) {
         ds.LinkedList list = new ds.LinkedList();
 
-        String[] devices = new String[] {"iMac", "iPhone 6S Plus","iPad", "Dell", "MacAir", "MacBook Pro"};
-        for (String device: devices) {
+        String[] devices = new String[]{"iMac", "iPhone 6S Plus", "iPad", "Dell", "MacAir", "MacBook Pro", "IBM NoteBook", "Windows Surface"};
+        for (String device : devices) {
             Node n = new Node(device);
             list.insert(n);
         }
         list.display();
+        // find a node
+        Node node = list.findNode("iPad");
+        if (node != null) {
+            System.out.println("\nFound Node:" + node.toString());
+        } else {
+            System.out.println("\nNot Found Node");
+        }
+        int size = list.size();
+        if (size > 2) {
+            for (int i = 1; i <= size - 2; i++) {
+                Node n = list.remove();
+                if (n != null) {
+                    System.out.println("\nRemoved Node:" + n.toString() + "\n");
+                } else {
+                    System.out.println("Linked List is Empty!");
+                }
+                list.display();
+            }
+            list.display();
+        }
     }
 }
--- java/ds/Node.java
+++ java/ds/Node.java
@@ -4,24 +4,63 @@ package ds;
  * Created by jules on 1/12/16.
  */
 public class Node {
+    private String device;
+    private Node next;
 
-        private String device;
-        private Node next;
+    /**
+     * Constructor for the Node
+     * @param pDevice
+     */
+    public Node(String pDevice) {
+        device = pDevice;
+        next = null;
 
-        public Node(String pDevice) {
+     }
 
-            device = pDevice;
-            next = null;
+    /**
+     * Override equal method
+     * @param n
+     * @return true or false
+     */
+    public boolean equals(Node n) {
+        return device.equals(n.getDevice());
+    }
 
-        }
-        public String toString() {
-            return device;
-        }
-        public String getDevice() { return device; }
-        public Node next() { return next; }
-        public void setNext( Node n) { next = n;}
-        public void displayNode() {
-            System.out.println("Node = " + getDevice());
-            System.out.println("    Next -> " + next().toString());
-        }
+    /**
+     * Another way to check if Node is equal
+     * @param dev
+     * @return true or false
+     */
+    public boolean equals(String dev) {
+        return device.equals(dev);
+    }
+    /**
+     * override toString for the object Node
+     * @return
+     */
+    public String toString() {
+        return device;
+    }
+
+    /**
+     * getter
+     * @return device
+     */
+    public String getDevice() { return device; }
+
+    /**
+     * Getter
+     * @return ref to the next node in the list
+     */
+    public Node next() { return next; }
+
+    /**
+     * setter
+     * @param n
+     */
+    public void setNext( Node n) { next = n;}
+    public void displayNode() {
+        System.out.println("Node = " + getDevice());
+        System.out.println("    Next -> " + next().toString());
+    }
 }

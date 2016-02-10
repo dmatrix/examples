15f5984e79040d0792424e37fc05df55a3e4b986
Jules S. Damji <dmatrix@comcast.net>
2016-02-09 22:20:48 -0800

    added header comments

--- spark/databricks/apps/scala/src/main/scala/iot/IoTDeviceDFApp.scala
+++ spark/databricks/apps/scala/src/main/scala/iot/IoTDeviceDFApp.scala
@@ -4,6 +4,12 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 /**
   * Created by jules on 2/9/16.
+  * This simple Spark app shows the utility and ease with which you can read and process a large dataset of JSON file as structured data.
+  * Using dataframes to represent a columnar structure, you can filter with predicates, select, and process data as though you were
+  * using a query langauge. Additionally, you can register the dataframe as a temporary table and then issue SQL like queries to it.
+  *
+  * All very easy and intuitive to use.
+  * 
   * spark-submit --class main.scala.iot.IoTDeviceDFApp --master local[6] target/scala-2.10/main-scala-iot_2.10-1.0.jar <path_to_json_file>
   */
 object IoTDeviceDFApp {
@@ -54,12 +60,15 @@ object IoTDeviceDFApp {
     df.registerTempTable("iot_devices_table")
 
     //issue select statements and then print the first 50 items from the results set
+    //think of results returned as rows of columns. In this case, two columns [string, string] from all rows
+    //that comprise the dataframe
     println("SELECT device_id, device_name FROM iot_devices_table")
     val results = sqlContext.sql("SELECT device_id, device_name FROM iot_devices_table")
     results.map(t => "Device Id: " + t(0) + " Device Name: " + t(1)).take(50).foreach(println)
     println("Total number of devices selected: " + results.count())
 
     //issue select statements and then print the first 50 items from the results set
+    //results returned as four columns of strings[device_id, device_name, humidity, temp]
     println("SELECT device_id, device_name, humidity, temp FROM iot_devices_table WHERE humidity  >= 85 AND temp <= 20")
     val results2 = sqlContext.sql("SELECT device_id, device_name, humidity, temp FROM iot_devices_table WHERE humidity  >= 85 AND temp <= 20")
     results2.map(t => "[Device Id: " + t(0) + " Device Name: " + t(1) + " Humidity: " +  t(2) + " Temp: " + t (3) + " ]").take(50).foreach(println)

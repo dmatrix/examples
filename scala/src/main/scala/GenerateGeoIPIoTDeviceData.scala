72d75a2f70d59c58fa9a50e165a9b226c01636d2
Jules S. Damji <dmatrix@comcast.net>
2016-03-13 14:44:11 -0700

    added output file for JSON data

--- scala/src/main/scala/GenerateGeoIPIoTDeviceData.scala
+++ scala/src/main/scala/GenerateGeoIPIoTDeviceData.scala
@@ -1,10 +1,14 @@
 package main.scala
 
+import java.io.{IOException, File, PrintWriter}
+
 import scala.collection.mutable.Map
 import scala.io.Source
 import DeviceProvision.{getHumidity, getRandomString, getTemperature, getRandomNumber}
 
 /**
+  * author: Jules S. Damji
+  * date: 3/12/2016
   * This short scala program generates simulated device data information for any IoT devices connected globally. It assusmes that
   * the device can generate events which are collected at a central collection agent that converts them into a JSON document
   * and publishes them onto to publish/subscribe networks such as PubNub or Apache Kafka.
@@ -13,11 +17,12 @@ import DeviceProvision.{getHumidity, getRandomString, getTemperature, getRandomN
   * can consume streams of these events as JSON documents and act on it in realtime for alters on high values of individual data points
   * such as temperature, humidity, battery power...etc
   *
-  * For visualization and analyses, I use this generated datasets in a Databricks Scala Notebooks.
+  * For visualization and analyses, I use generated datasets from this Scala program in a Databricks Scala Notebooks.
   */
 object GenerateGeoIPIoTDeviceData {
 
   val ccodes = Map[String, String] ()
+  val timeDelta = 150000
 
   def getDeviceName(id: Int): String = {
     var device: String = ""
@@ -44,17 +49,17 @@ object GenerateGeoIPIoTDeviceData {
       val cn = tokens(2)
       val latitude = tokens(len - 3).toDouble
       val longitude = tokens(len - 2).toDouble
-      //set cca2 and caa3 two unknown values if they don't exist
-      var cca3 = "N0P"
+      //set cca2 and caa3 to US and USA if a mapping doesn't exit
+      var cca3 = "USA"
       if (ccodes.contains(cca2))
         cca3 = ccodes(cca2)
       else
-        cca2 = "N0"
+        cca2 = "US"
       val deviceName = getDeviceName(id)
       val temperature = getTemperature()
       val humidity = getHumidity()
       val battery  = getRandomNumber(0, 10)
-      val timestamp: Long = System.currentTimeMillis / 1000
+      val timestamp: Long = System.currentTimeMillis + timeDelta
 
       djson = "{\"device_id\": %d, " +
         "\"device_name\": \"%s\", " +
@@ -75,14 +80,14 @@ object GenerateGeoIPIoTDeviceData {
 
   def main(args:Array[String]): Unit = {
 
-      if (args.length != 2) {
-        println("Need two files: <country_codes.txt> <ips_info.txt")
+      if (args.length != 3) {
+        println("Need two files: <country_codes.txt> <ips_info.txt> <ouput_path_to_json_file>")
         System.exit(2)
       }
 
     /**
-      * build the ISO-31661-1 2 letter country code to three letter country codes cca2-->caa3 mapping. For exmaple,
-      * NO -> NOR, PH -> PHL etc
+      * Build the ISO-31661-1 2 letter country code to three letter country codes cca2->caa3 mapping.
+      * For example, NO -> NOR, PH -> PHL etc
       */
     for (cline <- Source.fromFile(args(0)).getLines()) {
         val tokens = cline.split(" ")
@@ -90,13 +95,34 @@ object GenerateGeoIPIoTDeviceData {
           ccodes(tokens(0)) = tokens(1)
     }
     /**
-      * Read each line from the ip's info read from http://freegeoip.net/csv/<ip>, parse it and extract releveant
-      * info needed. For example, IP, cca2, country, latitute, and longitude
+      * Read each line from the ip's info read from http://freegeoip.net/csv/<ip>, parse it, and extract relevant
+      * info needed. For example, IP, cca2, country, latitude and longitude etc
       */
-    var id = 1
-    for (ipline <- Source.fromFile(args(1)).getLines()) {
-      println(toJSonDeviceData(ipline, id))
-      id += 1
+    var id = 0
+    var w: PrintWriter = null
+    try {
+      w = new PrintWriter(new File(args(2)))
+      for (ipline <- Source.fromFile(args(1)).getLines()) {
+        id += 1
+        val json = toJSonDeviceData(ipline, id)
+        if (json.length > 0) {
+          println(json)
+          w.write(json)
+          w.write("\n")
+        } else {
+          println("Failed to create JSON device data for " + ipline + " skipping...")
+        }
+      }
+    } catch {
+      case ex: IOException => {
+        ex.printStackTrace()
+        println("IO Exception")
+      }
+    } finally  {
+      if (id > 0)
+        println("JSON device file " + args(2) + " created with " + (id) + " devices.")
+      if (w != null)
+        w.close()
     }
   }
 }

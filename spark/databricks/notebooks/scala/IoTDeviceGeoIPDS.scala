e19dc6cad6c1d9441d0f8099e6c07bc9ebcd5c5f
Jules S. Damji <dmatrix@comcast.net>
2016-03-17 19:08:09 -0700

    Added commentary and related links and referrences

--- spark/databricks/notebooks/scala/IoTDeviceGeoIPDS.scala
+++ spark/databricks/notebooks/scala/IoTDeviceGeoIPDS.scala
@@ -1,11 +1,20 @@
-// Databricks notebook source exported at Fri, 18 Mar 2016 00:36:43 UTC
-// MAGIC %md ## How to Process IoT Device Data using Datasets and Dataframes - Part 2
+// Databricks notebook source exported at Fri, 18 Mar 2016 02:07:03 UTC
+// MAGIC %md ## How to Process IoT Device JSON Datasets using Datasets and Dataframes - Part 2
 
 // COMMAND ----------
 
-// MAGIC %md ![Simulation of IoT Connected Devices](https://github.com/dmatrix/examples/blob/master/confluent/images/kafka_pub_sub/kafka_pub_sub.001.jpeg "Simulating IoT Connected Devices")
+// MAGIC %md As in the [Part 1](http://bit.ly/1RhErbF), this notebook demonstrates the ease and simplicity of using Spark on the Databricks Cloud, without need to provisiom nodes, without need to manage clusters; all is done for you in this free [Databricks Community Edition](http://go.databricks.com/databricks-community-edition-beta-waitlist).
 // MAGIC 
-// MAGIC [Part 1] (http://bit.ly/1RhErbF) 
+// MAGIC With the introduction of [Dataframes](http://spark.apache.org/docs/latest/sql-programming-guide.html#dataframes) in Apache Spark 1.3 and [Datasets](https://databricks.com/blog/2016/01/04/introducing-spark-datasets.html) preview in 1.6, this notebook uses its APIs to show how you can quickly process structued data (JSON) with an inherent schema, intuitively compose relational queries using APIs, and, finally, against a temporary table, issue SQL like queries. By using notebook's myriad plotting options, the results from the SQL queries can be visualized for presenation and narration. Even better, these plots can be added to your dashboard.
+// MAGIC 
+// MAGIC In this second part, I have augmented the device dataset to include additional attributes, such as GeoIP locations, an idea borrowed from [AdTech Sample Notebook](https://cdn2.hubspot.net/hubfs/438089/notebooks/Samples/Miscellaneous/AdTech_Sample_Notebook_Part_1.html), as well as additional device attributes on which we can log alerts. Unlike the dataset size in [Part 1](http://bit.ly/1RhErbF), I uploaded close to 200K devices, curtailing from 2M entries to a smaller dataset.
+// MAGIC 
+// MAGIC Again, all code is availabe on my github:
+// MAGIC * [Python Scripts](https://github.com/dmatrix/examples/tree/master/py/ips)
+// MAGIC * [Scala Libraries](https://github.com/dmatrix/examples/tree/master/scala/src/main/scala)
+// MAGIC * [Scala Notebooks](https://github.com/dmatrix/examples/tree/master/spark/databricks/notebooks/scala)
+// MAGIC 
+// MAGIC Beside importing this notebook into your Databricks Cloud, you can also watch a [screencast]()
 
 // COMMAND ----------
 
@@ -19,7 +28,6 @@ import org.apache.spark.{SparkContext, SparkConf}
 // MAGIC Case class for mapping to a Dataset for the JSON {"device_id": 198164, "device_name": "sensor-pad-198164owomcJZ", "ip": "80.55.20.25", "cca2": "PL", "cca3": "POL", "cn": "Poland", "latitude": 53.080000, "longitude": 18.620000, "scale": "Celius", "temp": 21, "humidity": 65, "battery_level": 8, "c02_level": 1408, "lcd": "red", "timestamp" :1458081226051 }
 
 // COMMAND ----------
-
 
 //fetch the JSON device information uploaded into the Filestore
 val jsonFile = "/FileStore/tables/hll2y8jf1458081917577/iot_devices.json"
@@ -42,9 +50,9 @@ ds.take(10).foreach(println(_))
 
 // MAGIC %md Because the Dataset API in 1.6.1 is experimental, and not all relational functionality for writing complext expressions, is still under development, I have elect to convert Dataset to its equivalent Dataframe. Against this dataframe, I can now issue fairly in-depth relation expressions.
 // MAGIC 
-// MAGIC For all relationa expressions and queries, Tungsten will formulate an optimized logical and physical execute it.
+// MAGIC For all relational expressions, the [Catalyst](https://databricks.com/blog/2015/04/13/deep-dive-into-spark-sqls-catalyst-optimizer.html) will formulate an optimized logical and physical plan for execution, and [Tungsten](https://databricks.com/blog/2015/04/28/project-tungsten-bringing-spark-closer-to-bare-metal.html) engine will efficiently execute the optimized code. For our *DeviceIoTData*, it will use its standard encoders to optimize its binary internal representation, hence decrease the size of generate code, minimize the bytes transfered over the networks between nodes, and execute faster.
 // MAGIC 
-// MAGIC For instance, let's first filter the device dataset on temp and humidity predicate, convert the resulting Dataset into its Dataframe, and then select column by names, order by temperature in a descending order, and, finally, display the first 10 items.
+// MAGIC For instance, let's first filter the device dataset on *temp* and *humidity* attributes with a predicate, convert the resulting Dataset into its Dataframe, and then select column by names, order by temperature in a descending order, and, finally, display the first 10 items.
 
 // COMMAND ----------
 
@@ -55,7 +63,7 @@ val dsTemps = ds.filter(d => {d.temp > 30 && d.humidity > 70}).toDF().select("de
 
 // COMMAND ----------
 
-// MAGIC %md By contrast, here I don't convert to Dataframe but stick to Dataset APIs for filtering: take(10) returns an Array[DeviceIoTData]; using a foreach() method on the Array collection, I print each item.
+// MAGIC %md By contrast, in the next cell, I don't convert to Dataframe but use Dataset APIs for filtering: take(10) returns an Array[DeviceIoTData]; using a foreach() method on the Array collection, I print each item.
 
 // COMMAND ----------
 
@@ -89,7 +97,7 @@ val dsBatteryZero = ds.filter(d => {d.battery_level == 0}).toDF().select("device
 
 // COMMAND ----------
 
-// MAGIC %md Check the cluser logs for this alert
+// MAGIC %md Again, check the cluser logs for C02 alerts
 
 // COMMAND ----------
 
@@ -98,7 +106,7 @@ val dsHighC02Levels = ds.filter(d => {d.c02_level > 1400 && d.lcd == "red"}).toD
 
 // COMMAND ----------
 
-// MAGIC %md Since I'm having loads of fun and feeling adventerous, I'll attempt a more sophisticated relational query expression on the dataset. For instance, apply filter, convert to dataframe, groupBy, and compute average.
+// MAGIC %md Since I'm having loads of fun and feeling adventurous, I'll attempt a more sophisticated relational query expression on the dataset. For instance, string together a filter, convert to dataframe, groupBy, and compute average.
 
 // COMMAND ----------
 
@@ -108,9 +116,11 @@ dsGroupBy.show(10)
 
 // COMMAND ----------
 
-// MAGIC %md Finally, the fun bit. Data without visualization, withouth a narrative arch to infer insights or to see some problems is useless. We desire to make sense of the results.
+// MAGIC %md **Finally, the fun bit!**
 // MAGIC 
-// MAGIC By saving our Dataset, after converting to Dataframe, as a temporary table, I can issue more complext SQL queries against it and visualize the results, using notebook's myriad plotting options.
+// MAGIC Data without visualization, withouth a narrative arc, to infer insights or to see a trend is useless. We always desire to make sense of the results.
+// MAGIC 
+// MAGIC By saving our Dataset, after converting to Dataframe, as a temporary table, I can issue complex SQL queries against it and visualize the results, using notebook's myriad plotting options.
 
 // COMMAND ----------
 

d663ec9ab6865de9ce15ce15034cc5d2bdef331c
Jules S. Damji <dmatrix@comcast.net>
2016-03-20 13:59:55 -0700

    use partial applied function in the match structure

--- scala/src/main/scala/DeviceAlerts.scala
+++ scala/src/main/scala/DeviceAlerts.scala
@@ -2,45 +2,47 @@ package main.scala
 
 /**
   * Created by jules on 3/19/16.
+  * Singleton helper for device alerts.
   */
 object DeviceAlerts {
 
 
   def sendTwilio(message: String): Unit = {
-    //fill as necessary
+    //TODO: fill as necessary
     println("Twilio:" + message)
   }
 
   def sendSNMP(message: String): Unit = {
-    //fill as necessary
+    //TODO: fill as necessary
     println("SNMP:" + message)
   }
 
   def sendPOST(message: String): Unit = {
-    //fill as necessary
+    //TODO: fill as necessary
     println("HTTP POST:" + message)
   }
 
   def publishOnConcluent(message:String): Unit = {
-    //fill as necessary
+    //TODO: fill as necessary
     println("Kafka Topic 'DeviceAlerts':" + message)
   }
 
   def publishOnPubNub(message: String): Unit = {
-    //fill as necessary
+    //TODO: fill as necessary
     println("PubNub Channel 'DeviceAlerts':" + message)
   }
 
   def main(args: Array[String]): Unit = {
 
     for (arg <- args) {
-        arg match {
-          case "twilio" => sendTwilio("device 1 down!")
-          case "snmp" => sendSNMP("device 2 down!")
-          case "post" => sendPOST("device 3 down!")
-          case "kafka" => publishOnConcluent("device 4 down!")
-          case "pubnub" => publishOnPubNub("device 5 down")
+        val func = arg match {
+          case "twilio" => sendTwilio _
+          case "snmp" => sendSNMP _
+          case "post" => sendPOST _
+          case "kafka" => publishOnConcluent _
+          case "pubnub" => publishOnPubNub _
         }
+        func("Device 42 down!")
     }
   }
 }

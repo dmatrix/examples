933f4e50f5892a824deb8bb523c6dc93fc0ffb8d
Jules Damji <dmatrix@comcast.net>
2016-08-10 17:50:32 -0700

    added Hive and more queries

--- spark/databricks/apps/scala/2.0/src/main/scala/zips/SparkSessionSimpleZipExample.scala
+++ spark/databricks/apps/scala/2.0/src/main/scala/zips/SparkSessionSimpleZipExample.scala
@@ -5,6 +5,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 import org.apache.spark.sql.Encoder
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
 
 
 /**
@@ -30,10 +31,13 @@ object SparkSessionSimpleZipsExample {
     // You automatically get as part of the SparkSession
     // SparkSession uses the builder design to construct a SparkSession
 
+     val warehouseLocation = "file:${system:user.dir}/spark-warehouse"
+
      val spark = SparkSession
       .builder()
       .appName("SparkSessionZipsExample")
-      .config("spark.some.config.option", "some-value")
+      .config("spark.sql.warehouse.dir", warehouseLocation)
+      .enableHiveSupport()
       .getOrCreate()
 
     import spark.implicits._
@@ -49,6 +53,13 @@ object SparkSessionSimpleZipsExample {
     //filter all cities whose population > 40K
     zipsDF.filter(zipsDF.col("pop")> 40000).show(10)
 
+    // let's cache
+    zipsDF.cache()
+
+    //find the all populous cites and zip in the state of california and order them  by population
+
+    zipsDF.select("city", "zip", "pop").filter('state === "CA").orderBy(desc("pop")).show(10)
+
     //Now create an SQL table and issue SQL queries against it without
     // explicting using SQLContext but from SparkSession
     // Creates a temporary view using the DataFrame
@@ -56,7 +67,18 @@ object SparkSessionSimpleZipsExample {
 
     println("SELECT city, pop, state, zip FROM zips_table WHERE pop > 40000")
     val resultsDF = spark.sql("SELECT city, pop, state, zip FROM zips_table WHERE pop > 40000")
-     resultsDF.show(10)
+    resultsDF.show(10)
+
+    //drop the table if exists to get around existing tabel error
+    spark.sql("DROP TABLE IF EXISTS zips_hive_table")
+     //Now save as a hive table
+     spark.table("zips_table").write.saveAsTable("zips_hive_table")
+     // make a query to the hive table now
+     val resultsHiveDF = spark.sql("SELECT city, pop, state, zip FROM zips_hive_table WHERE pop > 40000")
+     resultsHiveDF.show(10)
+
+     //Find the populus cities in Calfornia with total number of zips using the hive table
+     spark.sql("SELECT COUNT(zip), SUM(pop), city FROM zips_hive_table WHERE state = 'CA' GROUP BY city ORDER BY SUM(pop) DESC").show(10)
   }
 }
 
